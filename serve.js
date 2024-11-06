4a5f450f0838dfe543b0f883f0b6c68d6eca4d42
Pawel Laba <labapawel@gmail.com>
2024-11-06 13:54:43 +0100

    zmiany

--- serve.js
+++ serve.js
@@ -1,6 +1,22 @@
 const express = require("express");
-const mysql = require("mysql");
-const path = require('path')
+const mysql = require("mysql2");
+const path = require('path');
+require('dotenv').config();
+
+let con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+con.connect( (err)=>{
+    if(err){
+        console.error("Błąd połączenia z bazą danych", err.message);
+    } else
+            console.log("Połączono z bazą danych");
+})
+
 
 let app = express();
 app.listen(5500, ()=>{
